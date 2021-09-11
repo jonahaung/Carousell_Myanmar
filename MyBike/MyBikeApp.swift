d0ba29382cb53d0449018688c6ca2bee2893d9ed
Aung Ko Min <jonahaung@gmail.com>
2021-09-12 04:35:09 +0800

    Initial Commit

---
+++ MyBike/MyBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBikeApp.swift
+//  MyBike
+//
+//  Created by Aung Ko Min on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MyBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
